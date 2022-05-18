1caa740fdd5bc8ade1b77bfccc4ed4d9a0151f8f
wshift <wtfshift@gmail.com>
2022-05-18 10:23:13 +0300

    fix: move parse text function to an external-api folder

--- src/external-api/oree.ts
+++ src/external-api/oree.ts
@@ -1,12 +1,20 @@
 import fetch from 'node-fetch';
 import { PRICE_PAGE_URL } from '../constants';
 import FormData from 'form-data';
+import jsdom from 'jsdom';
+const { JSDOM } = jsdom;
 
 interface PriceParamsType {
   date: string | string[];
   dateTo: string | string[];
   type: string | string[];
   market: string | string[];
+}
+
+enum Selectors {
+  nodesList = 'div.mpage-prices.mpage-prices-data.v2.active',
+  price = 'span.mpage-prices-type-value',
+  percentage = 'span.index-prices-value',
 }
 
 export const getPricePage = async ({
@@ -27,4 +35,22 @@ export const getPricePage = async ({
     body: formData,
   });
   return response.text();
+};
+
+const removeLinebreaks = (str: string) => {
+  return str.replace(/[\t\n]+/gm, '').trim();
+};
+
+export const parsePriceFromText = (text: string) => {
+  const domPage = new JSDOM(text);
+  const domNodes = domPage.window.document.querySelectorAll(
+    Selectors.nodesList
+  );
+  const price = removeLinebreaks(
+    domNodes[0].querySelector(Selectors.price).textContent
+  );
+  const percentage = removeLinebreaks(
+    domNodes[0].querySelector(Selectors.percentage).textContent
+  );
+  return { price, percentage };
 };
--- src/services/price-service.ts
+++ src/services/price-service.ts
@@ -1,7 +1,4 @@
-import { getPricePage } from '../external-api/oree';
-import jsdom from 'jsdom';
-
-const { JSDOM } = jsdom;
+import { getPricePage, parsePriceFromText } from '../external-api/oree';
 
 interface PriceListType {
   // price types from original https://www.oree.com.ua/
@@ -20,34 +17,10 @@ interface PriceRequestType {
   type: string | string[];
 }
 
-enum Selectors {
-  nodesList = 'div.mpage-prices.mpage-prices-data.v2.active',
-  price = 'span.mpage-prices-type-value',
-  percentage = 'span.index-prices-value',
-}
-
-const removeLinebreaks = (str: string) => {
-  return str.replace(/[\t\n]+/gm, '').trim();
-};
-
-const parsePriceFromText = (text: string) => {
-  const domPage = new JSDOM(text);
-  const domNodes = domPage.window.document.querySelectorAll(
-    Selectors.nodesList
-  );
-  const price = removeLinebreaks(
-    domNodes[0].querySelector(Selectors.price).textContent
-  );
-  const percentage = removeLinebreaks(
-    domNodes[0].querySelector(Selectors.percentage).textContent
-  );
-  return { price, percentage };
-};
-
 export const getPrices = async ({ date, dateTo, type }: PriceRequestType) => {
   try {
     const datesAndType = { date, dateTo, type };
-
+    // getPricePage returns html page as a string
     const rdnPageText = await getPricePage({
       ...datesAndType,
       market: 'DAM',
